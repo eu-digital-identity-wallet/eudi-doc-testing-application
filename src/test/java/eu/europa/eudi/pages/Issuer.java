b765e1855d4a66235764491d2eb2bf242c8efd98
ftheofil <ftheofil@netcompany.com>
2024-09-11 12:51:04 +0300

    android xpath changes

--- src/test/java/eu/europa/eudi/pages/Issuer.java
+++ src/test/java/eu/europa/eudi/pages/Issuer.java
@@ -382,7 +382,7 @@ public class Issuer {
     }
             public void clickAuthorize () {
                 if (test.getSystemOperation().equals(Literals.General.ANDROID.label)) {
-                    test.mobileWebDriverFactory().getWait().until(ExpectedConditions.elementToBeClickable(eu.europa.eudi.elements.android.WalletElements.authorize)).click();
+                    test.mobileWebDriverFactory().getWait().until(ExpectedConditions.visibilityOfElementLocated(eu.europa.eudi.elements.android.WalletElements.authorize)).click();
                 } else {
                     test.mobileWebDriverFactory().getWait().until(ExpectedConditions.elementToBeClickable(eu.europa.eudi.elements.ios.WalletElements.authorize)).click();
                 }
