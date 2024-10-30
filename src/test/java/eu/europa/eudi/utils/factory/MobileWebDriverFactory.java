59d8e5d6c34073d74fb212fd0ee86e90b1245122
Georgios Oraiopoulos <goraiopo@netcompany.com>
2024-10-30 13:49:26 +0200

    set increased android Install Timeout

--- src/test/java/eu/europa/eudi/utils/factory/MobileWebDriverFactory.java
+++ src/test/java/eu/europa/eudi/utils/factory/MobileWebDriverFactory.java
@@ -52,6 +52,7 @@ public class MobileWebDriverFactory {
         caps2.setCapability("allowInsecure", "adb_shell");
         caps2.setCapability("enableLogcatLogging", true);
         caps2.setCapability("adbExecTimeout", 2400000);
+        caps2.setCapability("androidInstallTimeout", 2400000);
 
         try {
             androidDriver = new AndroidDriver(new URL(test.envDataConfig().getAppiumUrlAndroid()), caps2);
