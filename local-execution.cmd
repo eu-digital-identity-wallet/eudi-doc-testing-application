af6e2209be1d72d4ef3d9aeca1975ec1fb8285dc
ftheofil <ftheofil@netcompany.com>
2025-01-16 12:19:53 +0200

    adding feature title to @US_SD

--- local-execution.cmd
+++ local-execution.cmd
@@ -1,2 +1,2 @@
-call mvn clean verify -Dcucumber.filter.tags="(@US_COCD_TC_01 and @ANDROID)" %*
+call mvn clean verify -Dcucumber.filter.tags="@US_IAVA_TC_01 and @ANDROID" %*
 call mvn serenity:aggregate
