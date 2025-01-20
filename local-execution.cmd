011a224aa57dbec31a8b858a335b9db74433a448
ftheofil <ftheofil@netcompany.com>
2025-01-20 10:22:22 +0200

    logs functionality

--- local-execution.cmd
+++ local-execution.cmd
@@ -1,2 +1,2 @@
-call mvn clean verify -Dcucumber.filter.tags="@manual and @ANDROID" %*
-call mvn serenity:aggregate
+call mvn clean verify -Dcucumber.filter.tags="@US_BRCD_TC_01 and @ANDROID" -Dserenity.threads=16 %*
+call mvn serenity:aggregate -Dserenity.threads=16
