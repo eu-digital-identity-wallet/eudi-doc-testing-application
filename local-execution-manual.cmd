dc7908f9cc171982431404333e244db63c91f8ae
ftheofil <ftheofil@netcompany.com>
2025-01-21 14:28:00 +0200

    adding file local-execution-manual.cmd

--- local-execution-manual.cmd
+++ local-execution-manual.cmd
@@ -1,2 +1,2 @@
-call mvn clean verify -Dcucumber.filter.tags="@manual" -Dserenity.threads=16 %*
+call mvn clean verify -Dcucumber.filter.tags="@US_BRCD_TC_01 and @manual" -Dserenity.threads=16 %*
 call mvn serenity:aggregate -Dserenity.threads=16
