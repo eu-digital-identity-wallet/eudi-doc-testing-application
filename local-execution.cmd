511619382fb5ceaf0b3391f799b68a17620c58ab
ftheofil <ftheofil@netcompany.com>
2025-01-23 10:26:42 +0200

    minor fixes for reporting

--- local-execution.cmd
+++ local-execution.cmd
@@ -1,2 +1,2 @@
-call mvn clean verify -Dcucumber.filter.tags="@manual" -Dserenity.threads=16 %*
-call mvn serenity:aggregate -Dserenity.threads=16
+call mvn clean verify -Dcucumber.filter.tags="@manual and @US_BRCD_TC_01" %*
+call mvn serenity:aggregate
