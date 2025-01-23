d6767b777cd27afd3e9d0d76f05c6885667d6b80
ftheofil <ftheofil@netcompany.com>
2025-01-23 14:55:01 +0200

    minor fixes for reporting

--- local-execution.sh
+++ local-execution.sh
@@ -1,3 +1,2 @@
-#!/usr/bin/env bash
-mvn clean verify -Dappium.hub=http://127.0.0.1:4723 -Dwebdriver.driver=appium -Dappium.platformName=Android -Dappium.deviceName="Pixel 6 API 33" -Dcucumber.filter.tags="@IOS and (not @WIP)"
-mvn serenity:aggregate
+call mvn clean verify -Dcucumber.filter.tags="@manual and @US_BRCD_TC_01" %*
+call mvn serenity:aggregate
