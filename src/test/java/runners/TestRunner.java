55da798924aa18573547054802842aade6a8e616
ftheofil <ftheofil@netcompany.com>
2025-04-23 16:50:06 +0300

    fixes on manual github actions

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -9,7 +9,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"src/test/resources/features/"},
         monochrome = true,
-        tags = "@Q1_2025",
         glue = {"eu.europa.eudi.stepdefs"})
 public class TestRunner {
 }
