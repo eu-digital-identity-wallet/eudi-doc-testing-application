187fe3d2add29e93357028e9915bba7c5375d51b
ftheofil <ftheofil@netcompany.com>
2025-04-24 14:11:08 +0300

    fixes on manual github actions

--- local-execution-mac.sh
+++ local-execution-mac.sh
@@ -2,4 +2,4 @@
 mvn clean verify -Dcucumber.filter.tags="@Q1_2025" "$@"
 rm -rf target/site/serenity
 # Clear previous report data
-mvn serenity:aggregate -Dtags=US_ADBSQRC
+mvn serenity:aggregate -Dtags=Q1_2025
--- local-execution.sh
+++ local-execution.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mvn clean verify -Dcucumber.filter.tags="@Q1_2025 and @ANDROID" "$@"
+mvn clean verify -Dcucumber.filter.tags="@Q1_2025" "$@"
 rm -rf target/site/serenity
-mvn serenity:aggregate -Dtags="Q1_2025 AND ANDROID"
+mvn serenity:aggregate -Dtags="Q1_2025"
