c1c57241e7e592fe6b3bd8f683db2b7be74c7fba
ftheofil <ftheofil@netcompany.com>
2025-05-14 13:40:20 +0300

    css changes on report

--- local-execution-mac.sh
+++ local-execution-mac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mvn clean verify -Dcucumber.filter.tags="@check" "$@"
+mvn clean verify -Dcucumber.filter.tags="@US_VP_TC_01 and @IOS" "$@"
 rm -rf target/site/serenity
 # Clear previous report data
 mvn serenity:aggregate -Dtags=check
