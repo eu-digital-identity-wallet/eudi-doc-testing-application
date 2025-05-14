acc58e4b9e181b1e837ba0551b78c313a9ff8bca
ftheofil <ftheofil@netcompany.com>
2025-05-14 14:31:59 +0300

    css changes on report

--- src/test/java/eu/europa/eudi/stepdefs/GeneralStepDefs.java
+++ src/test/java/eu/europa/eudi/stepdefs/GeneralStepDefs.java
@@ -922,7 +922,7 @@ public class GeneralStepDefs{
     public void theUserViewsTheDocumentThatIsRequested() {
         theUserHasSelectedSomeData();
         theUserUnselectsSomeOfThisData();
-        aCorrespondingMessageIsDisplayed();
+//        aCorrespondingMessageIsDisplayed();
         test.mobile().wallet().clickShareButton();
         test.mobile().verifier().insertPIN();
     }
@@ -955,7 +955,7 @@ public class GeneralStepDefs{
     public void theUserHasFinalizedDataSelection() {
         theUserHasSelectedSomeData();
         theUserUnselectsSomeOfThisData();
-        aCorrespondingMessageIsDisplayed();
+//        aCorrespondingMessageIsDisplayed();
     }
 
     @When("the user clicks the share button")
