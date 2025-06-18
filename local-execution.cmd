59f801603176a7f5278210ba1ced1d5b19afad9c
ftheofil <ftheofil@netcompany.com>
2025-06-18 15:35:04 +0300

    changes to yml file

--- local-execution.cmd
+++ local-execution.cmd
@@ -1,2 +1,3 @@
-call mvn clean verify -Dcucumber.filter.tags="(@US_VP_TC_01 and @ANDROID)" %*
-call mvn serenity:aggregate
+call mvn clean verify -Dcucumber.filter.tags="(@US_VP_TC_01 and @ANDROID) or (@US_BRCD_TC_01 and @ANDROID)"
+rmdir /s /q target/site/reports
+call mvn serenity:aggregate -Dtags="US_VP_TC_01 or US_BRCD_TC_01"
