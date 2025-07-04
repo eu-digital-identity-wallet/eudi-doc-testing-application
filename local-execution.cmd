2521d3b950c9ae2360b9393087695740b388a8b7
ftheofil <ftheofil@netcompany.com>
2025-07-04 12:17:09 +0300

    refactoring automated test scenarios

--- local-execution.cmd
+++ local-execution.cmd
@@ -1,3 +1,3 @@
-call mvn clean verify -Dcucumber.filter.tags="(@US_VP_TC_01 and @ANDROID) or (@US_BRCD_TC_01 and @ANDROID)"
+call mvn clean verify -Dcucumber.filter.tags="(@automated and @ANDROID)"
 rmdir /s /q target/site/reports
-call mvn serenity:aggregate -Dtags="US_VP_TC_01 or US_BRCD_TC_01"
+call mvn serenity:aggregate -Dtags="automated and ANDROID"
