8340ec300e33dd81561d91a87b758f8978e00b11
ftheofil <ftheofil@netcompany.com>
2025-07-23 16:12:08 +0300

    adding apk version for android

--- local-execution-mac.sh
+++ local-execution-mac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mvn clean verify -Dcucumber.filter.tags="@sakis" "$@"
+mvn clean verify -Dcucumber.filter.tags="@automated or @Q2_2025" "$@"
 rm -rf target/site/serenity
 # Clear previous report data
-mvn serenity:aggregate -Dtags=sakis
+mvn serenity:aggregate -Dtags="automated or Q2_2025"
