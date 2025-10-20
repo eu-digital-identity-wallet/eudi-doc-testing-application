6874450928e8c9bdfd72b339d98497658c7d9609
ftheofil <ftheofil@netcompany.com>
2025-10-20 10:43:55 +0300

    merge code from browserstack to develop

--- local-execution.cmd
+++ local-execution.cmd
@@ -1,3 +1,3 @@
-call mvn clean verify -Dcucumber.filter.tags="@US_BRCD_TC_01"
+call mvn clean verify -Dcucumber.filter.tags="@US_VP_TC_01"
 rmdir /s /q target/site/reports
-call mvn serenity:aggregate -Dtags="US_BRCD_TC_01"
+call mvn serenity:aggregate -Dtags="US_VP_TC_01"
