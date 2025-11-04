a31e07ec4c3d1ee77596bc79481f6c0df743d21f
ftheofil <ftheofil@netcompany.com>
2025-11-04 17:14:35 +0200

    adding githubactions functionality

--- local-execution.cmd
+++ local-execution.cmd
@@ -1,6 +1,2 @@
-TEST_CASES="@US_VP_TC_01 and @ANDROID"
-echo "Running Android tests with tags: $TEST_CASES"
-export CUCUMBER_FILTER_TAGS="$TEST_CASES"
-mvn test -ntp -Dapp.url=$BROWSERSTACK_APP_URL -Dcucumber.filter.tags="$TEST_CASES"
-cp src/test/resources/custom-style.css target/site/reports/EUDI_Wallet_Version_2025.10.31-Demo/css/core.css
-mvn serenity:aggregate -Dtags="@US_VP_TC_01 and @ANDROID"
+mvn verify -ntp -Dtest=TestRunner -Dcucumber.filter.tags="@US_VP_TC_01 and @ANDROID"
+mvn serenity:aggregate -Dtags="@US_VP_TC_01 AND @ANDROID"
