d8e8135620e449949c3dcf1dddf3b189a6805a8f
ftheofil <ftheofil@netcompany.com>
2025-11-04 16:29:29 +0200

    fixing logs for local real tests

--- local-execution.cmd
+++ local-execution.cmd
@@ -1,1 +1,6 @@
-call mvn serenity:aggregate -Dtags="automated or Q2_2025"
+TEST_CASES="@US_VP_TC_01 and @ANDROID"
+echo "Running Android tests with tags: $TEST_CASES"
+export CUCUMBER_FILTER_TAGS="$TEST_CASES"
+mvn test -ntp -Dapp.url=$BROWSERSTACK_APP_URL -Dcucumber.filter.tags="$TEST_CASES"
+cp src/test/resources/custom-style.css target/site/reports/EUDI_Wallet_Version_2025.10.31-Demo/css/core.css
+mvn serenity:aggregate -Dtags="@US_VP_TC_01 and @ANDROID"
