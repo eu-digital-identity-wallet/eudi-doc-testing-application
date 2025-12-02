efc4e03c1d73d48ffd7ec88935f0ffb59fb9ee67
ftheofil <ftheofil@netcompany.com>
2025-12-02 11:19:36 +0200

    adding latest ios fixes

--- local-execution-mac.sh
+++ local-execution-mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export CUCUMBER_FILTER_TAGS="@automated"
-mvn clean test -Dtest=TestRunner -Dcucumber.filter.tags="@automated" "$@"
+export CUCUMBER_FILTER_TAGS="@Q3_2025 or @automated"
+mvn clean test -Dtest=TestRunner -Dcucumber.filter.tags="@Q3_2025 or @automated" "$@"
 rm -rf target/site/serenity
 # Clear previous report data
-mvn serenity:aggregate -Dtags="automated"
+mvn serenity:aggregate -Dtags="Q3_2025 or automated"
