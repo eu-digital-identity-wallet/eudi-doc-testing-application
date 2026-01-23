8a4f484da5d5cde04b6d36f9f8615f3d24d79625
ftheofil <ftheofil@netcompany.com>
2026-01-23 09:52:01 +0200

    adding changes on pom versions

--- local-execution-mac.sh
+++ local-execution-mac.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export CUCUMBER_FILTER_TAGS="@report_analysis"
-mvn test -Dtest=TestRunner -Dcucumber.filter.tags="@report_analysis" "$@"
+export CUCUMBER_FILTER_TAGS="@report_analysis_with_status"
+mvn test -Dtest=TestRunner -Dcucumber.filter.tags="@report_analysis_with_status" "$@"
 rm -rf target/site/serenity
 # Clear previous report data
-mvn serenity:aggregate -Dtags="report_analysis"
+mvn serenity:aggregate -Dtags="report_analysis_with_status"
 echo "--- Applying custom CSS ---"
 
 REPORT_DIR="target/site/reports/EUDI_Wallet_Version_2025.12.34-Demo/css"
