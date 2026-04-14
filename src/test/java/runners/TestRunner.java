e2ad6ac0aef3e918f457e522fc19c0329ef16205
ftheofil <ftheofil@netcompany.com>
2026-04-14 17:16:09 +0300

    merge

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        features = {"src/test/resources/features/**"},
+        features = {"src/test/resources/features"},
         monochrome = true,
         glue = {"eu.europa.eudi.stepdefs"})
 public class TestRunner {
