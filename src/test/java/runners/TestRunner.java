10c9f9abec3b72e671796fc8556c225725420853
ftheofil <ftheofil@netcompany.com>
2026-06-30 16:04:43 +0300

    refactoring githubactions

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        features = {"src/test/resources/features"},
+        features = {"src/test/resources/features/android"},
         monochrome = true,
         glue = {"eu.europa.eudi.stepdefs"},
         plugin = {"rerun:target/rerun.txt"})
