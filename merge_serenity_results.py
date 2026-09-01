6d27748cb815c5d81d4970b1a5d8605d88d5d43e
ftheofil <ftheofil@netcompany.com>
2026-09-01 15:54:54 +0300

    replace py code

--- merge_serenity_results.py
+++ merge_serenity_results.py
@@ -5,7 +5,7 @@ import os
 import re
 import shutil
 
-RESULTS_DIR = "target/site/reports/EUDI_Wallet_Version_2026.07.39-Demo"
+RESULTS_DIR = "target/site/reports/EUDI_Wallet_Version_2026.08.41-Demo"
 BACKUP_DIR = "target/serenity-backup"
 
 PRIORITY = {
