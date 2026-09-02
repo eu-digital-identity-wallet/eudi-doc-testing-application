18cede97751a823e899845b18da8c4fcd8f24fa9
ftheofil <ftheofil@netcompany.com>
2026-09-02 09:31:29 +0300

    minor fixes on yml and chrome driver

--- src/test/java/eu/europa/eudi/utils/factory/WebWebDriverFactory.java
+++ src/test/java/eu/europa/eudi/utils/factory/WebWebDriverFactory.java
@@ -18,12 +18,6 @@ public class WebWebDriverFactory {
     public void startWebDriverSession() {
         ChromeOptions options = new ChromeOptions();
 
-        String chromeDriverPath = System.getenv("CHROMEDRIVER_PATH");
-
-        if (chromeDriverPath != null && !chromeDriverPath.isEmpty()) {
-            System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-        }
-
         webDriver = new ChromeDriver(options);
 
         wait = new WebDriverWait(
